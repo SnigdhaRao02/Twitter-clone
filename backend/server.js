95bda6c1e4de4fdd34d3e0f8b8976246c3b93466
skothar2 <skothar2@ncsu.edu>
2024-05-21 08:44:49 -0400

    Project setup done - connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "../backend/routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
+dotenv.config();
 const app=express();
 
-app.get('/', (req,res)=>{
-    res.send('server ready');
-});
+// app.get('/', (req,res)=>{
+//     res.send('server ready');
+// });
+
+app.use("/api/auth", authRoutes);
 
 
-app.listen(8000, ()=>{
+app.listen(process.env.POST || 8000, ()=>{
     console.log('server up and running at port 8000');
+    connectMongoDB();
 });
